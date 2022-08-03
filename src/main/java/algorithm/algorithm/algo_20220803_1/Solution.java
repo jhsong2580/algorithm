0803ebd71185638ffaa9f06c5bdffff9f32b88d0
jhsong2580 <thdwmdgns@naver.com>
2022-08-03 21:29:40 +0900

    algorithm: Container With Most Water

---
+++ src/main/java/algorithm/algorithm/algo_20220803_1/Solution.java
@@ -0,0 +1,25 @@
+package algorithm.algorithm.algo_20220803_1;
+
+import java.util.zip.CheckedInputStream;
+
+public class Solution {
+
+    public int maxArea(int[] height) {
+        int verticalCount = height.length;
+        int max = Integer.MIN_VALUE;
+        int frontIndex = 0;
+        int lastIndex = verticalCount -1;
+
+        while(lastIndex - frontIndex != 0){
+            int curHeight = Math.min(height[frontIndex], height[lastIndex]);
+            max = Math.max(curHeight * (lastIndex - frontIndex), max);
+            if(height[frontIndex] >= height[lastIndex]){
+                lastIndex--;
+            }else{
+                frontIndex++;
+            }
+        }
+        return max;
+    }
+
+}
