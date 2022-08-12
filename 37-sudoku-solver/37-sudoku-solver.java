db55419c41f10bf838f7f69c398669eef7acdcaa
jhsong2580 <thdwmdgns@naver.com>
2022-08-12 23:17:58 +0900

    Time: 8 ms (87.03%), Space: 40 MB (88.31%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -17,9 +17,6 @@ class Solution {
         if (matchCount == board.length * board.length) {
             return true;
         }
-        if (x == board.length) {
-            return false;
-        }
         if (board[x][y] != '.') {
             return dfs(board, next_x, next_y, matchCount + 1);
         }
