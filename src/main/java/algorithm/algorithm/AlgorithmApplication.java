f4254d6c4aafd9993f1da9af2ce744605b576343
jhsong2580 <thdwmdgns@naver.com>
2022-08-17 22:47:21 +0900

    algorithm: Next Permutation

--- src/main/java/algorithm/algorithm/AlgorithmApplication.java
+++ src/main/java/algorithm/algorithm/AlgorithmApplication.java
@@ -1,17 +1,20 @@
 package algorithm.algorithm;
 
 
-import algorithm.algorithm.algo_20220731_1.algo_1;
-import algorithm.algorithm.algo_20220731_2.Solution;
+import algorithm.algorithm.algo_20220817_1.Solution;
+import java.util.LinkedList;
+import java.util.List;
+import sun.awt.image.ImageWatched.Link;
 
 public class AlgorithmApplication {
 
-	public static void main(String[] args) {
-
-		Solution solution = new Solution();
-		int i = solution.totalNQueens(5);
-		System.out.println(i);
-
-	}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {3,2,1};
+        solution.nextPermutation(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 
 }
