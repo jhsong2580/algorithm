95edea8e3d2bac0f69c35405351343f6bea2e4b4
jhsong2580 <thdwmdgns@naver.com>
2022-09-05 22:16:49 +0900

    algorithm:  Spiral Matrix III

--- src/main/java/algorithm/algorithm/AlgorithmApplication.java
+++ src/main/java/algorithm/algorithm/AlgorithmApplication.java
@@ -1,19 +1,20 @@
 package algorithm.algorithm;
 
 
-import algorithm.algorithm.algo_20220817_1.Solution;
-import java.util.LinkedList;
-import java.util.List;
-import sun.awt.image.ImageWatched.Link;
+import algorithm.algorithm.algo_20220905_1.Solution;
 
 public class AlgorithmApplication {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {3,2,1};
-        solution.nextPermutation(arr);
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        int[][] ints = solution.spiralMatrixIII(
+            5,6,1,4
+        );
+        for (int[] anInt : ints) {
+            for (int i : anInt) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
         }
     }
 
