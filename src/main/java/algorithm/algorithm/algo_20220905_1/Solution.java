bb743a26e11d4d1d68236e0be1e6aae98d24b03b
jhsong2580 <thdwmdgns@naver.com>
2022-09-05 22:08:01 +0900

    algorithm:  Spiral Matrix III

---
+++ src/main/java/algorithm/algorithm/algo_20220905_1/Solution.java
@@ -0,0 +1,56 @@
+package algorithm.algorithm.algo_20220905_1;
+
+public class Solution {
+
+    private static int number = 1;
+
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        number = 0;
+        int[][] result = new int[rows * cols][cols];
+        result[number++] = new int[]{rStart, cStart};
+        int size = 1;
+        cStart++;
+        while (number < rows * cols) {
+            for (int i = cStart; i < cStart + size; i++) {
+                if (!isIn(rows, cols, rStart, i)) {
+                    continue;
+                }
+                result[number++] = new int[]{rStart, i};
+            }
+            cStart = cStart + size - 1;
+            rStart++;
+            for (int i = rStart; i < rStart + size; i++) {
+                if (!isIn(rows, cols, i, cStart)) {
+                    continue;
+                }
+                result[number++] = new int[]{i, cStart};
+            }
+            rStart = rStart + size - 1;
+            cStart--;
+            for (int i = cStart; i >= cStart - (size); i--) {
+                if (!isIn(rows, cols, rStart, i)) {
+                    continue;
+                }
+                result[number++] = new int[]{rStart, i};
+            }
+            cStart = cStart - (size);
+            rStart--;
+
+            for (int i = rStart; i >= rStart - (size); i--) {
+                if (!isIn(rows, cols, i, cStart)) {
+                    continue;
+                }
+                result[number++] = new int[]{i, cStart};
+            }
+            rStart = rStart - (size);
+            cStart++;
+
+            size = size + 2;
+        }
+        return result;
+    }
+
+    private boolean isIn(int maxRows, int maxCols, int curRow, int curCol) {
+        return curRow >= 0 && curRow < maxRows && curCol >= 0 && curCol < maxCols;
+    }
+}
