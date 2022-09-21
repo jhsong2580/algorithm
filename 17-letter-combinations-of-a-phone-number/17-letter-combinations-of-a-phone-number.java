91b7a54f0420020763a1b234348c92fc8e4bb0c5
jhsong2580 <thdwmdgns@naver.com>
2022-09-21 21:41:39 +0900

    Time: 2 ms (66.37%), Space: 42.9 MB (45.40%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,32 @@
+class Solution {
+        Map<Character, List<String>> sources;
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() == 0){
+            return new LinkedList<>();
+        }
+        sources = new HashMap<>();
+        sources.put('2', Arrays.asList(new String[]{"a", "b", "c"}));
+        sources.put('3', Arrays.asList(new String[]{"d", "e", "f"}));
+        sources.put('4', Arrays.asList(new String[]{"g", "h", "i"}));
+        sources.put('5', Arrays.asList(new String[]{"j", "k", "l"}));
+        sources.put('6', Arrays.asList(new String[]{"m", "n", "o"}));
+        sources.put('7', Arrays.asList(new String[]{"p", "q", "r", "s"}));
+        sources.put('8', Arrays.asList(new String[]{"t", "u", "v"}));
+        sources.put('9', Arrays.asList(new String[]{"w", "x", "y", "z"}));
+
+        List<String> results = new LinkedList<>();
+        dfs(digits, 0, results, "");
+        return results;
+    }
+    private void dfs(String digits, int index, List<String> results, String curString){
+        if(index == digits.length()){
+            results.add(curString);
+            return;
+        }
+        List<String> spellings = sources.get(digits.charAt(index));
+        for(int i=0; i< spellings.size(); i++){
+            dfs(digits, index + 1, results, curString + spellings.get(i));
+        }
+    }
+}
