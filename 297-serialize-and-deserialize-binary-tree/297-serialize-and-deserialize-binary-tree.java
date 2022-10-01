28e621f3f055b0ae92a940fc21dc33075a399dc1
jhsong2580 <thdwmdgns@naver.com>
2022-10-01 13:02:15 +0900

    Time: 68 ms (19.23%), Space: 54.1 MB (32.53%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,86 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        LinkedList<TreeNode> childs = new LinkedList<>();
+        if (root == null) {
+            return "";
+        }
+        childs.push(root);
+        LinkedList<String> result = new LinkedList<>();
+        while (childs.size() != 0) {
+            TreeNode node = childs.pop();
+            if(node == null){
+                result.add("null");
+                continue;
+            }
+            childs.addLast(node.left);
+            childs.addLast(node.right);
+
+            result.add(String.valueOf(node.val));
+        }
+        return result.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+                if (data.trim().equals("[]")) {
+            return null;
+        }
+                String[] sources = null;
+        try{
+            sources = data.substring(1, data.length()-1).trim().split(",");
+        }catch(Exception e){
+            return null;
+        }
+        
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        if(sources.length == 0){
+            return null;
+        }
+        TreeNode treeNode = makeTreeNode(sources[0]);
+        if (sources.length == 1) {
+            return treeNode;
+        }
+        nodes.add(treeNode);
+        boolean isLeft = true;
+        for(int i=1; i< sources.length; i++){
+            if(isLeft){
+                TreeNode node = nodes.getFirst();
+                node.left = makeTreeNode(sources[i]);
+                if(node.left != null){
+                    nodes.addLast(node.left);
+                }
+            }else{
+                TreeNode node = nodes.pop();
+                node.right = makeTreeNode(sources[i]);
+                if(node.right != null){
+                    nodes.addLast(node.right);
+                }
+            }
+            isLeft = !isLeft;
+        }
+        return treeNode;
+    }
+
+    private TreeNode makeTreeNode(String source){
+        if (source.trim().equals("null")) {
+            return null;
+        }
+        TreeNode treeNode = new TreeNode(Integer.parseInt(source.trim()));
+        return treeNode;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
