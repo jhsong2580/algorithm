8fe2e6407bedf3a493be2b5d47dd78f3b6dde118
jhsong2580 <thdwmdgns@naver.com>
2022-11-18 23:40:52 +0900

    palindrome-number

---
+++ src/main/java/algorithm/algorithm/algo_20221118_1/Solution.java
@@ -0,0 +1,18 @@
+package algorithm.algorithm.algo_20221118_1;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
